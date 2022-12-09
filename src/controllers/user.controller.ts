68381f68a0c1cc458c09a835be43b12fc6618a92
Roshan Acharya <acharyaroshan2357@gmail.com>
2022-12-09 11:52:03 +0545

    style: setup prettier and eslint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,21 +1,25 @@
-import { RequestHandler } from "express";
+import { type RequestHandler } from 'express';
 
-import User, { IUser } from "~/models/User";
-import { jwt, hash, asyncHandler, ErrorResponse } from "~/utils";
+import User, { type IUser } from '~/models/User';
+import { jwt, hash, asyncHandler, ErrorResponse } from '~/utils';
 
-const getPayLoad = (user: IUser) => {
-  return {
-    id: user.id,
-  };
-};
+const getPayLoad = (user: IUser) => ({
+  id: user.id,
+});
 
 const login = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
 
-  if (!user) return next(new ErrorResponse("Invalid Credentials", 400));
+  if (user == null) {
+    next(new ErrorResponse('Invalid Credentials', 400));
+    return;
+  }
 
   const match = await hash.compare(req.body.password, user.password);
-  if (!match) return next(new ErrorResponse("Invalid Credentails", 400));
+  if (!match) {
+    next(new ErrorResponse('Invalid Credentails', 400));
+    return;
+  }
 
   const payload = getPayLoad(user);
   const token = jwt.generate(payload);
@@ -24,8 +28,10 @@ const login = asyncHandler(async (req, res, next) => {
 });
 
 const signup: RequestHandler = asyncHandler(async (req, res, next) => {
-  if (await User.findOne({ email: req.body.email }))
-    return next(new ErrorResponse("User already exists", 400));
+  if ((await User.findOne({ email: req.body.email })) != null) {
+    next(new ErrorResponse('User already exists', 400));
+    return;
+  }
 
   const { name, email, password } = req.body;
 
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import "module-alias/register";
-import path from "path";
-import dotenv from "dotenv";
+import 'module-alias/register';
+import path from 'path';
+import dotenv from 'dotenv';
+
+import cors from 'cors';
+import morgan from 'morgan';
+import express from 'express';
+
+import routes from '~/routes';
+import { connectToDB } from '~/utils';
+import { handleError } from '~/middlewares';
 
 dotenv.config({
-  path: path.resolve(process.cwd(), ".env"),
+  path: path.resolve(process.cwd(), '.env'),
 });
-
-import cors from "cors";
-import morgan from "morgan";
-import express from "express";
-
-import routes from "~/routes";
-import { connectToDB } from "~/utils";
-import { handleError } from "~/middlewares";
 
 declare global {
   namespace Express {
@@ -29,8 +29,8 @@ declare global {
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
-app.use(morgan("dev"));
+app.use(cors({ origin: '*' }));
+app.use(morgan('dev'));
 
 app.use(routes);
 
@@ -38,7 +38,8 @@ app.use(handleError);
 
 async function main() {
   await connectToDB();
-  const PORT = process.env.PORT || 5000;
+  const PORT = typeof process.env.PORT === 'number' ? process.env.PORT : 5000;
+
   app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
 
