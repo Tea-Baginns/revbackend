108439decc66fcb43826ea4461b3b234de04ebfd
Roshan Acharya <acharyaroshan2357@gmail.com>
2022-12-17 12:16:04 +0545

    fix: fix parameter order in compare

---
+++ README.md
@@ -0,0 +1,4 @@
+Local Development
+
+- create .env and copy .env.example to .env
+- docker-compose up dev
--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -4,6 +4,6 @@ const generate = async (password: string) =>
   await argon2.hash(password, { hashLength: 10 });
 
 const compare = async (password: string, hash: string) =>
-  await argon2.verify(password, hash);
+  await argon2.verify(hash, password);
 
 export default { generate, compare };
